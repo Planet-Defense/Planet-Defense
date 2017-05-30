293a5e77c00d7219a6406f1c8ad729ae343c7e0c
Aron Janarv <aron.janarv@barnebys.com>
2017-05-30 13:24:07 +0200

    refactor

--- luaui/widgets/gui_chicken.lua
+++ luaui/widgets/gui_chicken.lua
@@ -528,14 +528,16 @@ function DeleteSpawnPanel()
 end
 
 function widget:IsAbove(x, y)
-  local hoverXMin = x1 + 110
-  local hoverYMin = y1 + 160
-  if not currentTimeSeconds or not gameInfo.gracePeriod then
+  -- pregame
+  if not currentTimeSeconds or not gameInfo.gracePeriod or not gameInfo or currentTimeSeconds < gameInfo.gracePeriod then
     return
   end
 
+  local hoverXMin = x1 + 110
+  local hoverYMin = y1 + 160
+
   -- within unitdefs text row in chicken box and grace passed and more than 0 squads spawned
-  if hoverXMin < x and y1 + 145 < y and x < x1 + 270 and y < hoverYMin and currentTimeSeconds > gameInfo.gracePeriod then
+  if hoverXMin < x and y1 + 145 < y and x < x1 + 270 and y < hoverYMin then
     local squadCountTable = GetUnitsInfo('Count', true)
     if #squadCountTable > 0 then
       DeleteSpawnPanel()
